77c08fc7f8d71a8cb475da49335963c2d347880c
dragen1860 <dragen@live.cn>
2018-01-12 20:15:18 +0800

    add save and load model, good version, add previous checkpoint performance verification

--- UnknownNet.py
+++ UnknownNet.py
@@ -123,7 +123,7 @@ class UnknownNet(nn.Module):
 		:return:
 		"""
 		assert input.size(0) == 1
-		if random.randint(1, 2000) < 2:
+		if random.randint(1, 4000) < 2:
 			need_print = True
 		else:
 			need_print = False
@@ -263,7 +263,33 @@ if __name__ == '__main__':
 	print('total params:', params)
 
 	num_cls = 64
-	for label_status in range(label_status_start, num_cls):
+	# 1. test previous model performance
+	for label_status in range(label_status_start + 1):
+		total = 500
+		right = 0
+		for i in range(total):
+			label_test = np.random.choice([label_status, num_cls - 1], 1, p=[0.5, 0.5])
+			if label_test == label_status:  # select training data
+				label_test = random.randint(0, label_status)
+				img = Variable(db.get_test(label_test).unsqueeze(0)).cuda()
+				pred, prob = net.predict(img)  # [1, 6]
+				pred = pred[0]
+				if pred == label_test:
+					right += 1
+			else:  # select unknown data
+				label_test = random.randint(label_status + 1, num_cls - 1)
+				img = Variable(db.get_test(label_test).unsqueeze(0)).cuda()
+				pred, prob = net.predict(img)  # [1, 6]
+				pred = pred[0]
+				if pred == -1:
+					right += 1
+
+		accuracy = right / total
+		print('>> verify learning:', label_status,  'accuracy:', accuracy)
+
+
+	# from label_status_start, exclusive to learn
+	for label_status in range(label_status_start + 1, num_cls):
 		step = 0
 		accuracy = 0
 		total_loss = 0
